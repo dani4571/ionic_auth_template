fad195bde3b312f7f21bbe4c3e2f1d2b8153b555
Daniel Curran <danielcurran90@gmail.com>
2018-02-07 18:33:34 -0600

    initial commit for ionic auth template

---
+++ README.md
@@ -0,0 +1,12 @@
+Authentication template for firebase.
+
+Need to change values in src/app/app.firebase.config.ts
+
+to recreate:
+1. ionic start auth_template blank
+2. cd auth_template
+3. npm install --save firebase
+4. import firebase from 'firebase' in app.component.ts
+
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,16 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+import { FIREBASE_CONFIG } from './app.firebase.config';
+import firebase from 'firebase';
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,6 +21,18 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+    });
+
+    firebase.initializeApp(FIREBASE_CONFIG);
+    
+    const unsubscribe = firebase.auth().onAuthStateChanged( user => {
+      if (!user) {
+        this.rootPage = 'LoginPage';
+        unsubscribe();
+      } else { 
+        this.rootPage = HomePage;
+        unsubscribe();
+      }
     });
   }
 }
